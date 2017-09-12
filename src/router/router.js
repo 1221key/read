c6d01ee3fec79f52b05c267db14671ba33ea739f
匡谭理 <962943593@qq.com>
2017-09-12 22:48:11 +0800

    add eslint

---
+++ src/router/router.js
@@ -0,0 +1,31 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// Vue.use(Router)
+
+// const First = { template: '<div>first</div>' }
+// const Second = { template: '<div>second</div>' }
+// const Home = { template: '<div>home</div>' }
+
+// const router = new Router({
+//     mode: 'history',
+//     base: __dirname,
+//     routers: [
+//         { path: '/', component: Home },
+//         { path: '/first', component: First },
+//         { path: '/second', component: Second }
+//     ]
+// })
+
+// new Vue({
+//     router,
+//     template: `
+//         <div id='r'>
+//             <h1>导航</h1>
+//             <ul>
+//                 <li><router-link to="/"></router-link></li>
+//                 <li><router-link to="/first"></router-link></li>
+//                 <li><router-link to="/second"></router-link></li>
+//             </ul>
+//         </div>
+//     `
+// }).$mount("#app")
