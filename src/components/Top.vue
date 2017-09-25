d3e117ffb5e07cea15bbc2cec7a873f2fbee97ef
匡谭理 <962943593@qq.com>
2017-09-25 14:59:23 +0800

    添加top组件

---
+++ src/components/Top.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="top">
+      <div>sdfsdfsdfef</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.top{height:50px;background: yellow;margin:0 auto;}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Top from '@/components/Top'
+
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'Hello',
-        component: Hello
-    }]
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: '/top',
+            name: 'Top',
+            component: Top
+        }
+    ]
 })
